ae62dfe719d3025f66f25e2190f1c34279354281
NerijusPazera <nerijus.pazera@gmail.com>
2019-10-29 17:29:09 +0200

    Uzduotis #5

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>PHP lydės ir <?php print date ('Y.m.d', strtotime('+1 day')); ?>!</title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <h1>
-            <b>Nerijus</b>- PHP su manim buvo ir <?php print date('H:i', strtotime('-1 hour')); ?>
-        </h1>
-        <p>
-            <?php print date('Y', strtotime('+1 year')); ?> ne už kalnų !
-        </p>
-    </body>
+        <h1>Dainu tekstai</h1>
+        <h2>Paragrafo Daina</h2>
+        <p>Aš toks vienišas, jaunas paragrafas...</p>
+        <p>Mindau svetimo kodo gatves...</p>
+    <body>
 </html>
