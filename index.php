3b2576638bc600bab5bd94b076ad4ab91facfa11
NerijusPazera <nerijus.pazera@gmail.com>
2019-10-29 17:56:06 +0200

    Uzduotis #7

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Komentarai</title>
+        <title>
+            PHP lydės ir <?php print date('Y', strtotime('+' . rand(0,10) . 'year'));?>
+        </title>
+    <h1>
+        <b>Nerijus</b>- galbūt turėsiu <?php print rand(1,5);?> vaikų(us)!
+    </h1>
+    <p>
+        D. Trump'as nebebus prezidentu: <?php print date('Y.m.d', strtotime('+' . rand(2,10) . 'year')); ?>
+    </p>
     </head>
     <body>
-        <h1>Dainu tekstai</h1>
-        <h2>Paragrafo Daina</h2>
-        <p>Aš toks vienišas, jaunas paragrafas...</p>
-        <!--<p>Mindau svetimo kodo gatves...</p>-->
+        
     <body>
 </html>
