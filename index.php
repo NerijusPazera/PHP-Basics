94fb5494a57321c62b58971bfa3ad6bb9620d44c
NerijusPazera <nerijus.pazera@gmail.com>
2019-10-29 16:07:16 +0200

    Uzduotis #2

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>As valdau HTML</title>
+    <title>Aš valdau HTML <?php print 'ir PHP'?>!</title>
   </head>
   <body>
-    <h1>Nerijus- HTML asas !</h1>
-    <p>Moku kurti statini turini</p>
+    <h1><b>Nerijus</b>- HTML<?php print 'ir PHP'?> asas !</h1>
+    <p>Moku kurti statinį turinį.<br>Atidaryta:<?php print date("Y.m.d H:i:s")?></p>
   </body>
 </html>
