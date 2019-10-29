32c9c79c6fd26683d3861ccbb9bd33f9ee09ddc3
NerijusPazera <nerijus.pazera@gmail.com>
2019-10-29 15:14:07 +0200

    Uzduotis #1

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-print 'lol';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>As valdau HTML</title>
+  </head>
+  <body>
+    <h1>Nerijus- HTML asas !</h1>
+    <p>Moku kurti statini turini</p>
+  </body>
+</html>
