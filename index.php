459f38f3aee069e72e35cfdbc134945db12bb7d7
NerijusPazera <nerijus.pazera@gmail.com>
2019-10-30 14:57:08 +0200

    Uzduotis #8

--- index.php
+++ index.php
@@ -2,17 +2,26 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>
-            PHP lydės ir <?php print date('Y', strtotime('+' . rand(0,10) . 'year'));?>
-        </title>
-    <h1>
-        <b>Nerijus</b>- galbūt turėsiu <?php print rand(1,5);?> vaikų(us)!
-    </h1>
-    <p>
-        D. Trump'as nebebus prezidentu: <?php print date('Y.m.d', strtotime('+' . rand(2,10) . 'year')); ?>
-    </p>
+        <title>PHP</title>
+        <style>
+            body {
+                background-color: rgb(<?php print rand(0, 255) ?>,<?php print rand(0, 255) ?>,<?php print rand(0, 255) ?>);
+            }
+            h1 {
+                font-size: <?php print rand(16, 30) ?>px;
+            }
+            p {
+                color: rgb(<?php print rand(0, 255) ?>,<?php print rand(0, 255) ?>,<?php print rand(0, 255) ?>);
+            }
+        </style>
     </head>
     <body>
-        
+        <h1>
+            Aš keičiu savo dydi!
+        </h1>
+
+        <p>
+            Aš keičiu savo spalvą!
+        </p>
     <body>
 </html>
