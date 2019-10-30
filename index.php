82695f66aed1c76edda0430c022a4e82dc988a9f
NerijusPazera <nerijus.pazera@gmail.com>
2019-10-30 17:02:44 +0200

    Uzduotis XI

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                 flex-direction:column;
                 align-items:center;
             }
-            .bomb {
+            .bomb-<?php print date('s'); ?> {
                 height: <?php print date('s'); ?>vh;
                 width: <?php print date('s'); ?>vw;
                 background-image: url("https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png");
@@ -17,10 +17,17 @@
                 background-repeat: no-repeat;
                 background-position: center;
             }
+            .bomb-00 {
+                background-image: url("https://gamespot1.cbsistatic.com/uploads/original/gamespot/images/2004/features/tenspot/fps/554342-fps_013.jpg");
+                height: 70vh;
+                width: 70vw;
+                background-size: contain;
+                background-position: center;
+            }
         </style>
     </head>
     <body>
-        <div class="bomb"></div>
+        <div class="bomb-<?php print date('s'); ?>"></div>
         <div class="sec"><?php print date('s'); ?></div>
     </body>
 </html>
