e1a9a084c3d57086053ce97f03976ad9c64e5d4e
NerijusPazera <nerijus.pazera@gmail.com>
2019-11-03 10:35:37 +0200

    Uzduotis Variables #6

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
-$distance = rand(10, 300);
+
+$distance = rand(10, 5000);
 $consumption = 7.5;
 $price_l = 1.3;
 $fuel_total = round($consumption / 100 * $distance, 2);
 $price_trip = round($fuel_total * $price_l, 2);
 $my_money = 100;
+
 if ($price_trip > $my_money) {
     $isvada = 'Neiperkama';
 } else {
     $isvada = 'Iperkama';
 }
+
 $h1 = 'Keliones skaiciuokle';
 $li_1 = "Nuvaziuota distancija: $distance";
 $li_2 = "Sunaudota: $fuel_total l. kuro";
 $li_3 = "Kaina: $price_trip pinigu";
 $li_4 = "Turimi pinigai $my_money";
 $p = "Isvada: Kelione $isvada";
+
 ?>
 <html>
     <head>
