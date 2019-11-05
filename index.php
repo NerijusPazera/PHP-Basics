d4b18c29ea23e9d2e45dff09df3c88d239709b3f
NerijusPazera <nerijus.pazera@gmail.com>
2019-11-05 16:15:32 +0200

    Uzduotis ciklai wallet2

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
+
 $months = 12;
 $wallet = 1000;
 $month_income = 700;
 
 for ($x = 1; $x <= $months; $x++) {
-    $month_expenses = rand(300, 700);
+    $month_expenses = rand(300, 1200);
     $wallet += $month_income - $month_expenses;
+    if ($wallet < 0) {
+        $h2 = "Atsargiai, $x mėnesį gali baigtis pinigai !";
+        break;
+    } else {
+        $h2 = "Po $months m., prognozuojamas likutis: $wallet";
+    }
 }
 
 $h1 = 'Wallet Forecast';
-$h2 = "Po $months m., prognozuojamas likutis: $wallet";
 
 ?>
 
