f9c83109094d3fda632ed47312dcf574615b7b35
NerijusPazera <nerijus.pazera@gmail.com>
2019-11-05 17:36:12 +0200

    Uzduotis ciklai cizos

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php
+$days = 365;
+$pack_price = 3.50;
+$count_ttl = 0;
+$time_per_cig = 5;
 
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
+for ($d = 0; $d < $days; $d++) {
+    $weekday = date('N', strtotime("+$d day"));
 
-for ($x = 1; $x <= $months; $x++) {
-    $month_expenses = rand(300, 1200);
-    $wallet += $month_income - $month_expenses;
-    if ($wallet < 0) {
-        $h2 = "Atsargiai, $x mėnesį gali baigtis pinigai !";
-        break;
+    if ($weekday == 6) {
+        $cigs_sat = rand(10, 20);
+        $count_ttl += $cigs_sat;
+    } elseif ($weekday == 7) {
+        $cigs_sun = rand(1, 3);
+        $count_ttl += $cigs_sun;
     } else {
-        $h2 = "Po $months m., prognozuojamas likutis: $wallet";
+        $cigs_mon_fri = rand(3, 4);
+        $count_ttl += $cigs_mon_fri;
     }
 }
 
-$h1 = 'Wallet Forecast';
+$price_ttl = ceil($count_ttl / 20) * $pack_price;
+$time_ttl = round($count_ttl * $time_per_cig / 60);
 
+$h1 = 'Mano dumu skaiciuokle';
+$h2 = "Per $days dienas, surukysiu $count_ttl cigareciu uz $price_ttl eur.";
+$h3 = "Viso traudamas prastovėsiu $time_ttl valandų."
 ?>
-
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title><?php print $h1; ?></title>
-    </head>
+<html>
     <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
-</htm
+</html>
