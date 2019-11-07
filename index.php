7de8ae40419b7f6b6f9366e61450dffee8244b80
NerijusPazera <nerijus.pazera@gmail.com>
2019-11-07 15:06:55 +0200

    Uzduotis array #1

--- index.php
+++ index.php
@@ -1,28 +1,21 @@
-<?php 
+<?php
 
-$speed_of_sound = 333;
-$max_db = 120;
-$db_decr = 6;
-$db = 120;
-$p = '';
+$array = [
+    [
+        'pavadinimas' => 'Kiausiniai',
+        'kiekis' => 2,
+        'dydis' => 'dideli'
+    ],
+    [
+        'pavadinimas' => 'Baklazanas',
+        'kiekis' => 1,
+        'dydis' => 'mazas'
+    ],
+    [
+        'pavadinimas' => 'Grietines indelis',
+        'kiekis' => 1,
+        'dydis' => 'didelis'
+    ],
+];
 
-for ($dist = 1; $db > 0; $dist *= 2) {
-    $db -= $db_decr;
-    $sec = round($dist / $speed_of_sound);
-    $p .= "Po $sec s ($dist)m: $db db <br><br>";
-}
-
-$h1 = 'Griaustinio zona';
-
-?>
-
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title><?php print $h1; ?></title>
-    </head>
-    <body>
-        <h1><?php print $h1; ?></h1>
-        <p><?php print $p; ?></p>
-    </body>
-</html>
+var_dump($array);
